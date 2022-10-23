7fc90823215beb17a271a9e665cfc31d3fa9fbc5
mortvvnutri <93926034+mortvvnutri@users.noreply.github.com>
2022-10-23 15:22:18 +0300

    Add files via upload

---
+++ Models/Theachers.cs
@@ -0,0 +1,13 @@
+﻿namespace VUS.Models
+{
+    public class Theachers
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Discipline { get; set; }
+        public string Facult { get; set; }
+        //public string Schedule { get; set; }
+    }
+}
---
+++ Models/UpdateStudentsRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace VUS.Models
+{
+    public class UpdateStudentsRequest
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Group { get; set; }
+        public string Facult { get; set; }
+    }
+}
